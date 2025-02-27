e8e3bdb9045968b9c11db1bb36fee1c7c0093a5e
saw mon <sawmon71293@gmail.com>
2025-02-28 00:28:12 +0700

    first commit

---
+++ customize.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", async function () {
+    try {
+
+        const response = await fetch("your-api-endpoint", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer your-access-token",             },
+        });
+
+        if (!response.ok) throw new Error("Failed to fetch purchase data");
+	const cartItems = window.checkoutData?.products || [];
+        const data = await response.json();
+        // Extract purchase data
+        const purchases = data?.message?.data || [];
+
+              const hasRecurringProduct = purchases.some(purchase => 
+            purchase.billingCycleType === "RECURRING"
+        );
+
+        if (hasRecurringProduct) {
+            displayRecurringCheckbox();
+        }
+
+    } catch (error) {
+        console.error("Error fetching purchase data:", error);
+    }
+});
+
+// Function to create and display the checkbox
+function displayRecurringCheckbox() {
+    const radioContainer = document.getElementById("#i2bfyo"); // Change this to the correct container
+	console.log ('radio container >>>>',radioContainer)
+}
