235ebeb3ffd5d190cbc647079944d15c9be11d79
saw mon <sawmon71293@gmail.com>
2025-02-28 09:40:41 +0700

    commented

--- customize.js
+++ customize.js
@@ -1,30 +1,30 @@
 document.addEventListener("DOMContentLoaded", async function () {
     try {
 
-        const response = await fetch("your-api-endpoint", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": "Bearer your-access-token",             },
-        });
+    //     const response = await fetch("your-api-endpoint", {
+    //         method: "GET",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             "Authorization": "Bearer your-access-token",             },
+    //     });
 
-        if (!response.ok) throw new Error("Failed to fetch purchase data");
-	const cartItems = window.checkoutData?.products || [];
-        const data = await response.json();
-        // Extract purchase data
-        const purchases = data?.message?.data || [];
+    //     if (!response.ok) throw new Error("Failed to fetch purchase data");
+	// const cartItems = window.checkoutData?.products || [];
+    //     const data = await response.json();
+    //     // Extract purchase data
+    //     const purchases = data?.message?.data || [];
 
-              const hasRecurringProduct = purchases.some(purchase => 
-            purchase.billingCycleType === "RECURRING"
-        );
+    //           const hasRecurringProduct = purchases.some(purchase => 
+    //         purchase.billingCycleType === "RECURRING"
+    //     );
 
         if (hasRecurringProduct) {
             displayRecurringCheckbox();
         }
 
-    } catch (error) {
-        console.error("Error fetching purchase data:", error);
-    }
+    // } catch (error) {
+    //     console.error("Error fetching purchase data:", error);
+    // }
 });
 
 // Function to create and display the checkbox
