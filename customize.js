dd7cf72a827cf5dba7cface675a356b56217c1e0
saw mon <sawmon71293@gmail.com>
2025-03-01 13:21:42 +0700

    add items

--- customize.js
+++ customize.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', async function () {
+    const cartItems = []
     try {
         const response = await fetch("https://api.checkoutchamp.com/order/surcharge/?loginId=123&password=12345&mode=CALC&cardNumber=4809035904248714&shipPostalCode=30078&postalCode=30092&product1_id=1");
 
@@ -6,15 +7,14 @@ document.addEventListener('DOMContentLoaded', async function () {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
 
-        const data = await response.json();  // Change to .json() if API returns JSON
-        console.log("API Response: >>>>>>", data);
-        return data;  // Now you can use this data elsewhere
+       cartItems = await response.json();  // Change to .json() if API returns JSON
+
     } catch (error) {
         console.error("Error fetching data:", error);
     }
     // Function to check for recurring products
     function checkForRecurringProduct(cartItems) {
-    return cartItems.some(item => item.fulfillmentCycleType === 'RECURRING');
+        return cartItems.some(item => item.fulfillmentCycleType === 'RECURRING');
     }
 
     // Show or hide checkbox based on recurring products
